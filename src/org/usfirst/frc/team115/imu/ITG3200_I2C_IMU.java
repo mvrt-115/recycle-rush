0e6cef017a3fa989b32df48a935501d4c9ec9ab3
Lee Mracek <lee.mracek@gmail.com>
2015-01-07 14:47:09 -0800

    Can now read instantaneous angular velocity and linear acceleration

--- src/org/usfirst/frc/team115/imu/ITG3200_I2C_IMU.java
+++ src/org/usfirst/frc/team115/imu/ITG3200_I2C_IMU.java
@@ -1,15 +1,16 @@
 package org.usfirst.frc.team115.imu;
 
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.SensorBase;
 
-public class ITG3200_I2C_IMU extends SensorBase {
+public class ITG3200_I2C_IMU {
 	I2C i2c;
 	private static final byte ADDRESS = 0x68;
 	private static final byte SAMPLE_RATE_ADDRESS = 0x15;
-	private static final byte SAMPLE_RATE = 0x09;
 	private static final byte LOWPASS_FILTER_ADDRESS = 0x16;
-	private static final byte LOWPASS_FILTER_98HZ = 0x1a;
+	private static final byte FS_SEL = 0x18;
+	private static final byte DATA_REGISTER = 0x1D;
+	private static final double LSB_PER_DEGREESECOND = 14.375;
+	private Filter FILTER = Filter.k98Hz;
 	
 	public static class Axes {
 		public final byte value;
@@ -25,11 +26,84 @@ public class ITG3200_I2C_IMU extends SensorBase {
 			this.value = value;
 		}
 	}
-	
-	public ITG3200_I2C_IMU(I2C.Port port) {
+	/**
+	 * 
+	 * @param port
+	 * @param rate Rate in Hz
+	 * @param filter
+	 */
+	public ITG3200_I2C_IMU(I2C.Port port, int rate, Filter filter) {
 		i2c = new I2C(port, ADDRESS);
 		
-		i2c.write(SAMPLE_RATE_ADDRESS, SAMPLE_RATE);
-		i2c.write(LOWPASS_FILTER_ADDRESS, LOWPASS_FILTER_98HZ);
+		setSampleRate(rate);
+		setFilter(filter);
+		
+	}
+	
+	public void setSampleRate(int rate) {
+	    int temp = FILTER == Filter.k256Hz ? 8000/rate - 1 : 1000/rate - 1;
+	    if(temp > 255)
+	    	temp = 255;
+	    if(temp < 0) {
+	    	temp = 0;
+	    }
+		i2c.write(SAMPLE_RATE_ADDRESS, temp);
+	}
+	
+	public void setFilter(Filter filter) {
+		byte val = 2;
+		switch(filter) {
+		case k256Hz:
+			val = 0;
+			break;
+		case k188Hz:
+			val = 1;
+			break;
+		case k98Hz:
+			val = 2;
+			break;
+		case k42Hz:
+			val = 3;
+			break;
+		case k20Hz:
+			val = 4;
+			break;
+		case k10Hz:
+			val = 5;
+			break;
+		case k5Hz:
+			val = 6;
+			break;
+		}
+		i2c.write(LOWPASS_FILTER_ADDRESS, FS_SEL | val);
+	}
+	
+	public double getX() {
+		return getRate(Axes.X);
+	}
+	
+	public double getY() {
+		return getRate(Axes.Y);
+	}
+	
+	public double getZ() {
+		return getRate(Axes.Z);
+	}
+	
+	public double getRate(Axes axis) {
+		byte[] raw = new byte[2];
+		i2c.read(DATA_REGISTER + axis.value, raw.length, raw);
+		
+		return (short) ((raw[0] << 8 ) | raw[1]) / LSB_PER_DEGREESECOND;
+	}
+	public enum Filter {
+		k256Hz,
+		k188Hz,
+		k98Hz,
+		k42Hz,
+		k20Hz,
+		k10Hz,
+		k5Hz
+		
 	}
 }
