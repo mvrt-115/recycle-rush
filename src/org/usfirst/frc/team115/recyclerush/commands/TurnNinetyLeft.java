9574f1349048c11fa7ce3ccf8b53adf596569bef
Arushi Rai <arushirai1999@gmail.com>
2015-01-22 18:19:07 -0600

    Added two commands to turn 90 degrees each way

---
+++ src/org/usfirst/frc/team115/recyclerush/commands/TurnNinetyLeft.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team115.recyclerush.commands;
+
+import org.usfirst.frc.team115.recyclerush.Robot;
+
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+
+public class TurnNinetyLeft extends PIDCommand {
+
+	double desiredAngle;
+	Gyro gyro;
+
+	public TurnNinetyLeft(double p, double i, double d) {
+		super(p, i, d);
+
+		gyro = new Gyro(0);
+		this.setSetpoint(desiredAngle);
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		return gyro.getAngle();
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		Robot.drive.drive(Robot.oi.getJoystick().getY(), output);
+	}
+
+	@Override
+	protected void initialize() {
+		desiredAngle = returnPIDInput() - 90;
+	}
+	
+	@Override
+	protected void execute() {}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		Robot.drive.stop();
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+	
+}
