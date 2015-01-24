a9e0b702ffcc02bf0833ce869e15bf9fd4035dea
Michelangelo <chungmin99@gmail.com>
2015-01-23 18:46:22 -0600

    Fixed @m3rcuriel 's complaints

--- src/org/usfirst/frc/team115/recyclerush/commands/Turn.java
+++ src/org/usfirst/frc/team115/recyclerush/commands/Turn.java
@@ -7,47 +7,71 @@ import edu.wpi.first.wpilibj.command.PIDCommand;
 
 public class Turn extends PIDCommand {
 
-	private static final double K = 90;
-	double desiredAngle;
-	Gyro gyro;
-	
+	private double desiredAngle; // the final angle we strive for
+	private Gyro gyro;	// Creates a Gyro object
+	private double goal;
 
-	public Turn(double p, double i, double d) {
-		super(p, i, d);
-
+	/**
+	 * Constructor
+	 * @param p, proportional coefficient
+	 * @param i, integral coefficient
+	 * @param d, derivative coefficient
+	 */
+	public Turn(double goal) {
+		super(0, 0, 0);
 		gyro = new Gyro(0);
+		
+		this.goal = goal;
 	}
 
+	/**
+	 * @return the gyro's current angle
+	 */
 	@Override
 	protected double returnPIDInput() {
 		return gyro.getAngle();
 	}
-
+	
+	/**
+	 * Drives the robot forward at the joystick's speed, and at the output angle.
+	 * @param output angle to turn
+	 */
 	@Override
 	protected void usePIDOutput(double output) {
-		Robot.drive.drive(Robot.oi.getJoystick().getX(), output);
+		Robot.drive.drive(Robot.oi.getJoystick().getY(),  output);
 	}
 
 	@Override
-	protected void initialize() {}
+	protected void initialize() {
+		desiredAngle = goal + gyro.getAngle(); // Goal degree.
+	}
 	
 	@Override
 	protected void execute() {
-		desiredAngle = returnPIDInput() + (Robot.oi.getJoystick().getX()*K);
-		this.setSetpoint(desiredAngle);
-
 	}
-
+	
+	/**
+	 * @returns whether the current angle is within 2 degrees of desired angle
+	 */
 	@Override
 	protected boolean isFinished() {
+		if(Math.abs(desiredAngle - gyro.getAngle()) <= 2){
+			return true;
+		}
 		return false;
 	}
 
+	/**
+	 * Stops the robot
+	 */
 	@Override
 	protected void end() {
 		Robot.drive.stop();
 	}
-
+	
+	/**
+	 * Calls end()
+	 */
 	@Override
 	protected void interrupted() {
 		end();
