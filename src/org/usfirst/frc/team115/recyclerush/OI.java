a0541b850e5eb31d2cdba842d3e2a3bb3bee4206
Ishan Mitra <ishanbubby@gmail.com>
2015-01-27 15:51:03 -0800

    Added code to read from xbox

--- src/org/usfirst/frc/team115/recyclerush/OI.java
+++ src/org/usfirst/frc/team115/recyclerush/OI.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team115.recyclerush;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.Trigger;
 
 /**
  * @author Lee Mracek
@@ -8,18 +11,34 @@ import edu.wpi.first.wpilibj.Joystick;
  *         including Cypress, Joystick, Triggers, etc.
  */
 public class OI {
-    private Joystick joystick;
+	
+	private Joystick joystick;
+	private Joystick xbox;
+	
+	public OI() {
+		joystick = new Joystick(RobotMap.JOYSTICK);
+		xbox = new Joystick(RobotMap.XBOX);
+	}	
+	
+	public Joystick getJoystick() {
+		return joystick;
+	}
+	
+	public Joystick getXbox() {
+		return xbox;
+	}
+	
+	public boolean getXboxButton(int btn){
+		return xbox.getRawButton(btn);
+	}
+	
+	public double getXboxAxis(int axis){
+		return xbox.getRawAxis(axis);
+	}
+	
+	public int getXboxPOV(){
+		return xbox.getPOV();
+	}
 
-    public OI() {
-
-        joystick = new Joystick(RobotMap.JOYSTICK);
-
-        //todo: map buttons for commands (@akhil99)
-    }
-
-
-    public Joystick getJoystick() {
-        return joystick;
-    }
 }
 
--- src/org/usfirst/frc/team115/recyclerush/RobotMap.java
+++ src/org/usfirst/frc/team115/recyclerush/RobotMap.java
@@ -14,13 +14,33 @@ public class RobotMap {
     public static final int FRONT_LEFT_DRIVE = 2;
     public static final int FRONT_RIGHT_DRIVE = 9;
 
-    // joystick
-    public static final int JOYSTICK = 0;
-
     // solenoids
     public static final int CLAW_SOLENOID_1 = 0;
     public static final int CLAW_SOLENOID_2 = 1;
 
     public static final int GRABBER_SOLENOID_1 = 2;
     public static final int GRABBER_SOLENOID_2 = 3;
+
+    //joystick
+    public static final int JOYSTICK 			 = 0;
+    public static final int XBOX                 = 1;
+    
+    //xbox buttons
+    public static final int XBOX_A			 = 1;
+    public static final int XBOX_B			 = 2;
+    public static final int XBOX_Y			 = 4;
+    public static final int XBOX_X           = 3;
+    public static final int XBOX_LB			 = 5;
+    public static final int XBOX_RB			 = 6;
+    public static final int XBOX_BACK		 = 7;
+    public static final int XBOX_START 		 = 8;
+    public static final int XBOX_LAXIS_PRESS = 9;
+    public static final int XBOX_RAXIS_PRESS = 10;
+    public static final int XBOX_RT		 	 = 3;
+    public static final int XBOX_LT			 = 2;
+    public static final int XBOX_AXIS_LX 	 = 0;
+    public static final int XBOX_AXIS_LY	 = 1;
+    public static final int XBOX_AXIS_RX	 = 4;
+    public static final int XBOX_AXIS_RY 	 = 5;
+    
 }
