85752328c5b65444e53d6e37ac5c855cc472a956
Lee Mracek <lee.mracek@gmail.com>
2015-01-31 18:02:23 -0800

    Switched time based to timeout

--- src/org/usfirst/frc/team115/recyclerush/commands/DriveStraight.java
+++ src/org/usfirst/frc/team115/recyclerush/commands/DriveStraight.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.command.PIDCommand;
 public class DriveStraight extends PIDCommand {
 		
 	private double displacement;
-	private long stopTime = -1;
 	
 	public DriveStraight(double distance, double p, double i, double d) {
 		this(p, i, d);
@@ -18,7 +17,7 @@ public class DriveStraight extends PIDCommand {
 	
 	public DriveStraight(long ms, double p, double i, double d) {
 		this(p, i, d);
-		stopTime = System.currentTimeMillis() + ms;
+		this.setTimeout((float) (ms / 1000));
 	}
 	
 	public DriveStraight(double p, double i, double d) {
@@ -35,11 +34,13 @@ public class DriveStraight extends PIDCommand {
 	@Override
 	protected void usePIDOutput(double output) {
 		Robot.drive.drive((Robot.drive.getMode() == CANTalon.ControlMode.Position) ? displacement : Robot.oi.getJoystick().getY(), output);
-		System.out.println("use PID Output");
 	}
 
 	@Override
 	protected void initialize() {
+		if (Robot.drive.getMode() == CANTalon.ControlMode.Position) {
+			Robot.drive.enableControl();
+		}
 		this.setSetpoint(Robot.drive.getYaw());
 	}
 	
@@ -50,10 +51,7 @@ public class DriveStraight extends PIDCommand {
 
 	@Override
 	protected boolean isFinished() {
-		if (stopTime > 0) {
-			return System.currentTimeMillis() >= stopTime;
-		}
-		return false;
+		return isTimedOut();
 	}
 	
 	@Override
