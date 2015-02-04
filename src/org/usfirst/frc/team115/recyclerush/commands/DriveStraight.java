f49f405d273ec2b29e4d97c9ca59cedd70985231
Michael Sandler <jokegred@gmail.com>
2015-02-03 17:18:12 -0800

    added compressor smart dashboard

--- src/org/usfirst/frc/team115/recyclerush/commands/DriveStraight.java
+++ src/org/usfirst/frc/team115/recyclerush/commands/DriveStraight.java
@@ -4,27 +4,31 @@ import org.usfirst.frc.team115.recyclerush.Robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveStraight extends PIDCommand {
 		
 	private double displacement;
-	private boolean time = false;
+	private boolean timeUsed = false;
 	
 	public DriveStraight(double distance, double p, double i, double d) {
 		this(p, i, d);
+		timeUsed = false;
 		Robot.drive.setMode(CANTalon.ControlMode.Position);
 		displacement = distance;
+		SmartDashboard.putNumber("DriveDistance", displacement);
+		SmartDashboard.putBoolean("TimeUsed", timeUsed);
 	}
 	
 	public DriveStraight(long ms, double p, double i, double d) {
 		this(p, i, d);
 		this.setTimeout((float) (ms / 1000));
-		time = true;
+		timeUsed = true;
 	}
 	
 	public DriveStraight(double p, double i, double d) {
 		super(p, i, d);
-		time = false;
+		timeUsed = false;
 		requires(Robot.drive);
 		Robot.drive.setMode(CANTalon.ControlMode.PercentVbus);
 	}
@@ -36,7 +40,7 @@ public class DriveStraight extends PIDCommand {
 
 	@Override
 	protected void usePIDOutput(double output) {
-		if(time) {
+		if(timeUsed) {
 			Robot.drive.drive(1, output);
 		} else {
 			Robot.drive.drive((Robot.drive.getMode() == CANTalon.ControlMode.Position) ? displacement : Robot.oi.getJoystick().getY(), output);
