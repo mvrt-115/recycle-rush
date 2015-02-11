5bb68646369b610d1fa88190321a094247c1d695
Chung Min Kim <chungmin99@gmail.com>
2015-02-10 17:04:56 -0800

    DriveForDistance is addedd comments. Unnecessary lines in OI are edited. Minor changes made to previous commit.

--- src/org/usfirst/frc/team115/recyclerush/OI.java
+++ src/org/usfirst/frc/team115/recyclerush/OI.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team115.recyclerush;
 
-import org.usfirst.frc.team115.recyclerush.commands.ArcadeDriveWithJoystick;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class contains all interactions between physical controls and the robot,
@@ -12,12 +9,9 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  */
 public class OI {
     private Joystick joystick;
-    private JoystickButton driveStraightButton, timeButton;
 
     public OI() {
         joystick = new Joystick(RobotMap.JOYSTICK);
-        driveStraightButton = new JoystickButton(joystick, RobotMap.DRIVE_STRAIGHT_BUTTON);
-        timeButton = new JoystickButton(joystick, 2);
     }
 
 
