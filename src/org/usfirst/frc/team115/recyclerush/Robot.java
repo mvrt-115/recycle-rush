c3b81be4b94ff1e99501b1916c5b5bd478d19fe6
heatherabaker <heather@heatherbaker.com>
2015-02-14 10:25:20 -0800

    Added voltage ramping

--- src/org/usfirst/frc/team115/recyclerush/Robot.java
+++ src/org/usfirst/frc/team115/recyclerush/Robot.java
@@ -25,6 +25,10 @@ public class Robot extends IterativeRobot {
         grabber = new Grabber();
         oi = new OI();
     }
+    
+    public void robotInit() {
+    	drive.initialize();
+    }
 
     public void disabledPeriodic() {
         Scheduler.getInstance().run();
