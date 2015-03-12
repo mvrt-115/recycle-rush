b8c7e3e61516f8b4e29c302b439b47cbcbbba01f
Lee Mracek <lee.mracek@gmail.com>
2015-03-11 23:33:25 -0700

    Removed comments from JuggernautB

--- src/org/usfirst/frc/team115/recyclerush/commands/auton/JuggernautB.java
+++ src/org/usfirst/frc/team115/recyclerush/commands/auton/JuggernautB.java
@@ -27,42 +27,6 @@ public class JuggernautB extends CommandGroup{
 		addSequential(new RollerOpen());
 		addSequential(new OpenClaw());	
 		addSequential(new DriveStraightDistanceNoPID(-1.0));
-//		// addParallel(new RollerOpen());
-//
-//		// addParallel(new DriveForDistance(0.2));
-//
-//		addSequential(new ElevatorToHeight(Elevator.PRESET_TOTE_INTAKETOTE + 7));
-//		addParallel(new OpenClaw());
-//		addSequential(new CloseClaw());
-//		// addSequential(new ToggleStabilizer());
-//		addSequential(new ElevatorUp());
-//		addSequential(new ElevatorUp());
-//		addSequential(new DriveForDistance(2.1));
-//		addSequential(new ElevatorDown());
-//		addSequential(new ElevatorDown());
-//		addParallel(new OpenClaw());
-//		addSequential(new ToggleStabilizer());
-//		addSequential(new AutoIntake());
-//		addParallel(new DriveForDistance(2.75));
-//		addSequential(new ElevatorUp());
-//
-//		addSequential(new JuggernautRotate());
-//		addSequential(new DriveForDistance(4.1));
-//		addSequential(new AutoIntake());
-//		addParallel(new DriveForDistance(2.75));
-//		addSequential(new ElevatorUp());
-//
-//		addSequential(new JuggernautRotate());
-//		addParallel(new DriveForDistance(4.1));
-//		addSequential(new AutoIntake());
-//		addSequential(new Turn(90.0));
-//		//addSequential(new DriveForDistance(10.0));
-//		addParallel(new ElevatorDown());
-//		addSequential(new RollerOpen());
-//		addSequential(new OpenClaw());
-//		addParallel(new ToggleStabilizer());
-//		addSequential(new DriveForDistance(-1.0));
-
 	}
 
 }
