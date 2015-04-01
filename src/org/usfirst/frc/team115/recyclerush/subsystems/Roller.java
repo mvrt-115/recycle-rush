ae89d70ee38f80b4320d826295060f7b94d64de6
Donatello <akhil@theakhil.com>
2015-03-31 17:05:16 -0700

    Inverted roller

--- src/org/usfirst/frc/team115/recyclerush/subsystems/Roller.java
+++ src/org/usfirst/frc/team115/recyclerush/subsystems/Roller.java
@@ -4,7 +4,6 @@ import org.usfirst.frc.team115.recyclerush.RobotMap;
 import org.usfirst.frc.team115.recyclerush.commands.RollerArcadeWithJoystick;
 
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -14,40 +13,36 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Roller extends Subsystem {
 
-	private CANTalon leftMotor, rightMotor;
-	private RobotDrive rollerDrive;
+    private CANTalon leftMotor, rightMotor;
+    private RobotDrive rollerDrive;
 
-	public Roller() {
-		leftMotor = new CANTalon(RobotMap.ROLLER_MOTOR_LEFT);
-		rightMotor = new CANTalon(RobotMap.ROLLER_MOTOR_RIGHT);
+    public Roller() {
+        leftMotor = new CANTalon(RobotMap.ROLLER_MOTOR_LEFT);
+        rightMotor = new CANTalon(RobotMap.ROLLER_MOTOR_RIGHT);
 
-		rollerDrive = new RobotDrive(leftMotor, rightMotor);
+        rollerDrive = new RobotDrive(leftMotor, rightMotor);
 
-		rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-		rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
 
-		leftMotor.enableLimitSwitch(false, false);
-		rightMotor.enableLimitSwitch(false, false);
-	}
+        leftMotor.enableLimitSwitch(false, false);
+        rightMotor.enableLimitSwitch(false, false);
+    }
 
-	public void control(double move, double rotate) {
-		rollerDrive.arcadeDrive(move, rotate);
-	}
+    public void control(double move, double rotate) {
+        rollerDrive.arcadeDrive(move, -rotate);
+    }
 
-	public void control(Joystick joystick, int moveAxis, int rotateAxis) {
-		rollerDrive.arcadeDrive(joystick, moveAxis, joystick, rotateAxis);
-	}
+    public void stop() {
+        rollerDrive.arcadeDrive(0, 0);
+    }
 
-	public void stop() {
-		rollerDrive.arcadeDrive(0, 0);
-	}
+    public void log() {
+        // TODO find something to log
+    }
 
-	public void log() {
-		// TODO find something to log
-	}
-
-	@Override
-	protected void initDefaultCommand() {
-		setDefaultCommand(new RollerArcadeWithJoystick());
-	}
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new RollerArcadeWithJoystick());
+    }
 }
