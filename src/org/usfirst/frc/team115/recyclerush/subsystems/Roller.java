943a59b13dc01ee3f5f28dfad2202df917936e10
Akhil Palla <akhil@theakhil.com>
2015-04-16 14:24:27 -0700

    Removed inversion of roller for turn (my own control preferences)

--- src/org/usfirst/frc/team115/recyclerush/subsystems/Roller.java
+++ src/org/usfirst/frc/team115/recyclerush/subsystems/Roller.java
@@ -13,36 +13,36 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Roller extends Subsystem {
 
-    private CANTalon leftMotor, rightMotor;
-    private RobotDrive rollerDrive;
+	private CANTalon leftMotor, rightMotor;
+	private RobotDrive rollerDrive;
 
-    public Roller() {
-        leftMotor = new CANTalon(RobotMap.ROLLER_MOTOR_LEFT);
-        rightMotor = new CANTalon(RobotMap.ROLLER_MOTOR_RIGHT);
+	public Roller() {
+		leftMotor = new CANTalon(RobotMap.ROLLER_MOTOR_LEFT);
+		rightMotor = new CANTalon(RobotMap.ROLLER_MOTOR_RIGHT);
 
-        rollerDrive = new RobotDrive(leftMotor, rightMotor);
+		rollerDrive = new RobotDrive(leftMotor, rightMotor);
 
-        rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-        rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+		rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+		rollerDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
 
-        leftMotor.enableLimitSwitch(false, false);
-        rightMotor.enableLimitSwitch(false, false);
-    }
+		leftMotor.enableLimitSwitch(false, false);
+		rightMotor.enableLimitSwitch(false, false);
+	}
 
-    public void control(double move, double rotate) {
-        rollerDrive.arcadeDrive(move, -rotate);
-    }
+	public void control(double move, double rotate) {
+		rollerDrive.arcadeDrive(move, rotate);
+	}
 
-    public void stop() {
-        rollerDrive.arcadeDrive(0, 0);
-    }
+	public void stop() {
+		rollerDrive.arcadeDrive(0, 0);
+	}
 
-    public void log() {
-        // TODO find something to log
-    }
+	public void log() {
+		// TODO find something to log
+	}
 
-    @Override
-    protected void initDefaultCommand() {
-        setDefaultCommand(new RollerArcadeWithJoystick());
-    }
+	@Override
+	protected void initDefaultCommand() {
+		setDefaultCommand(new RollerArcadeWithJoystick());
+	}
 }
