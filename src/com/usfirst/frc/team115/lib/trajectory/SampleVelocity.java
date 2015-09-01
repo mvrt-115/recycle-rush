86db34289dd016cc053d6375403d468c2b552ef5
Heather Baker <heather@heatherbaker.com>
2015-09-01 18:06:36 -0500

    Bug fixes
    Removed dx = 0 from SampleVelocity

--- src/com/usfirst/frc/team115/lib/trajectory/SampleVelocity.java
+++ src/com/usfirst/frc/team115/lib/trajectory/SampleVelocity.java
@@ -7,77 +7,76 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SampleVelocity extends Command {
 
-	double dt = 1; //ms
+    double dt = 1; //ms
 
-	double elapsedT = 0;
-	double elapsedX = 0;
+    double elapsedT = 0;
+    double elapsedX = 0;
 
-	double initialTime = 0;
-	double instVel = 0;
-	double instAcc = 0;
-	double avgVel = 0;
-	double avgAcc = 0;
-	double maxVel = 0;
-	double maxAcc = 0;
-	double pastVel = 0;
+    double initialTime = 0;
+    double instVel = 0;
+    double instAcc = 0;
+    double avgVel = 0;
+    double avgAcc = 0;
+    double maxVel = 0;
+    double maxAcc = 0;
+    double pastVel = 0;
 
-	double totAccErr = 0;
-	double totVelErr = 0;
+    double totAccErr = 0;
+    double totVelErr = 0;
 
-	private static int ENCODER_SCALE = 1444;
-	private static double ERROR = 1;
-	private final double SCALE = (1 / (Math.PI * 8)) * 2 * ENCODER_SCALE / ERROR; //ticks per inch
+    private static int ENCODER_SCALE = 1444;
+    private static double ERROR = 1;
+    private final double SCALE = (1 / (Math.PI * 8)) * 2 * ENCODER_SCALE / ERROR; //ticks per inch
 
-	public SampleVelocity() {
-		setTimeout(7);
-		this.initialTime = Timer.getFPGATimestamp();
+    public SampleVelocity() {
+        setTimeout(7);
+        this.initialTime = Timer.getFPGATimestamp();
 
-	}
+    }
 
-	@Override
-	protected void initialize() {
-		Robot.drive.resetEncoders();
-		this.dt = 20;
-		this.dx = 0;
-	}
+    @Override
+    protected void initialize() {
+        Robot.drive.resetEncoders();
+        this.dt = 20;
+    }
 
-	@Override
-	protected void execute() {
-		this.dt = Timer.getFPGATimestamp() - this.elapsedT; //This is the unupdated elapsed time from the last loop
-		this.elapsedT = Timer.getFPGATimestamp() - this.initialTime;
-		this.elapsedX = Robot.drive.getDistance()/SCALE;
+    @Override
+    protected void execute() {
+        this.dt = Timer.getFPGATimestamp() - this.elapsedT; //This is the unupdated elapsed time from the last loop
+        this.elapsedT = Timer.getFPGATimestamp() - this.initialTime;
+        this.elapsedX = Robot.drive.getDistance()/SCALE;
 
-		this.instVel = Robot.drive.getVelocity();
-		this.avgVel = elapsedX/elapsedT;
+        this.instVel = Robot.drive.getVelocity();
+        this.avgVel = elapsedX/elapsedT;
 
-		this.instAcc = (instVel-pastVel)/dt;
-		this.avgAcc = (this.avgAcc*(elapsedT-dt) + instAcc*dt)/elapsedT;
+        this.instAcc = (instVel-pastVel)/dt;
+        this.avgAcc = (this.avgAcc*(elapsedT-dt) + instAcc*dt)/elapsedT;
 
-		this.maxAcc = maxAcc > instAcc ? instAcc : maxAcc;
-		this.maxVel = maxVel > instVel ? instVel : maxVel;
+        this.maxAcc = maxAcc > instAcc ? instAcc : maxAcc;
+        this.maxVel = maxVel > instVel ? instVel : maxVel;
 
-		this.log();
-	}
+        this.log();
+    }
 
-	@Override
-	protected boolean isFinished() {
-		return isTimedOut();
-	}
+    @Override
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
 
-	@Override
-	protected void end() {
-	}
+    @Override
+    protected void end() {
+    }
 
-	protected void log() {
-		SmartDashboard.putNumber("Velocity", instVel);
-		SmartDashboard.putNumber("Avg Velocity", avgVel);
-		SmartDashboard.putNumber("Max Velocity", maxVel);
+    protected void log() {
+        SmartDashboard.putNumber("Velocity", instVel);
+        SmartDashboard.putNumber("Avg Velocity", avgVel);
+        SmartDashboard.putNumber("Max Velocity", maxVel);
 
-		//SmartDashboard.putNumber("Acceleration", instVel);
-		//SmartDashboard.putNumber("Avg Acceleration", avgAcc);
-		//SmartDashboard.putNumber("Acceleration", maxAcc);
-	}
-	@Override
-	protected void interrupted() {
-	}
+        //SmartDashboard.putNumber("Acceleration", instVel);
+        //SmartDashboard.putNumber("Avg Acceleration", avgAcc);
+        //SmartDashboard.putNumber("Acceleration", maxAcc);
+    }
+    @Override
+    protected void interrupted() {
+    }
 }
